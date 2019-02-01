89f8aa434f716d45da90fc37a703d0e006fb56e9
Bitcoinrent Official <44014496+bitcoinrent@users.noreply.github.com>
2019-02-01 02:09:44 -0800

    Add files via upload

---
+++ src/noui.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2013-2014 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOINRENT_NOUI_H
+#define BITCOINRENT_NOUI_H
+
+extern void noui_connect();
+
+#endif // BITCOINRENT_NOUI_H
